1023c6830b46cca641f5b7e94b3b70ec412033e7
Brendan Leu <banakin9@gmail.com>
2020-12-20 14:16:31 -0500

    Change styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,38 @@
 <script lang='typescript'>
   // Import components
-  import Navbar       from './pages/Navbar.svelte';
-  import ScrollToTop  from './pages/ScrollToTop.svelte';
+  // import Navbar       from './pages/Navbar.svelte';
+  // import ScrollToTop  from './pages/ScrollToTop.svelte';
 
-  // Import pages
-  import Home      from './pages/Home.svelte';
-  import About     from './pages/About.svelte';
-  import Projects  from './pages/Projects.svelte';
-  import Pricing   from './pages/Pricing.svelte';
-  import Contact   from './pages/Contact.svelte';
+  // // Import pages
+  // import Home      from './pages/Home.svelte';
+  // import About     from './pages/About.svelte';
+  // import Projects  from './pages/Projects.svelte';
+  // import Pricing   from './pages/Pricing.svelte';
+  // import Contact   from './pages/Contact.svelte';
 
-  import * as data from './data/About.json';
-  const test = data.languages.TypeScript;
+  // import * as data from './data/About.json';
+  // const test = data.languages.TypeScript;
+  const test = "test";
 </script>
   
 <style lang="scss">
   // Import SCSS
   @import './styles/variables.scss';
+
+  :global(body){
+    font-family: 'Roboto', sans-serif;
+    background-color: $black;
+    color: $white;
+  }
 </style>
   
 <h1>{test}</h1>
 
-<Navbar />
+<!-- <Navbar />
 <ScrollToTop />
 
 <Home />
 <About />
 <Projects />
 <Pricing />
-<Contact />
+<Contact /> -->
