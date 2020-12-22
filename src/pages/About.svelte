258093156b3ace6b998ddb234098a0567c15aebb
Brendan Leu <banakin9@gmail.com>
2020-12-21 21:55:55 -0500

    Finish about

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -32,12 +32,39 @@
     @import '../styles/section';
 
     section {
-        font-weight: 600;
+        font-weight: 500 !important;
+        padding: 0;
 
-        div {
-            width: 100%;
+        @media (max-width: $breakpoint-md) {
+            padding: 2rem 0;
+        }
             
-            &.grid-container {
+        h1 {
+            font-weight: inherit;
+            font-size: 4rem;
+            font-style: normal;
+
+            margin: 0;
+            margin-bottom: 1rem;
+            padding: 0;
+        }
+
+        .page-section {
+            width: 100%;
+            margin-bottom: 5rem;
+
+            &:last-child {
+                margin-bottom: 0;
+            }
+
+            p {
+                font-size: 1.75rem;
+                margin: 0;
+                margin-bottom: 1rem;
+                padding: 0;
+            }
+
+            .grid-container {
                 column-count: 2;
                 column-gap: 10%;
 
@@ -46,13 +73,31 @@
                 }
             }
         }
+
+        .icon-group {
+            grid-area: social;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            flex-wrap: wrap;
+
+            @media (max-width: $breakpoint-md) {
+                justify-content: center;
+            }
+
+            a {
+                margin: 0 10px;
+            }
+        }        
     }
 </style>
 
 
 <section id={pageTitle}>
     <h1>About Me</h1>
-    <div>
+    <div class="page-section">
         <p>My confidence with languages</p>
         <div class="grid-container">
             {#each progress as bar, i}
@@ -64,7 +109,7 @@
             {/each}
         </div>
     </div>
-    <div>
+    <div class="page-section">
         <p>Tools im familiar with</p>
         <div class="icon-group">
             <a href={links.github}>   <Icon scale={iconScale} data={faGithub} />   </a>
@@ -73,6 +118,17 @@
             <a href={links.twitter}>  <Icon scale={iconScale} data={faTwitter} />  </a>
             <a href={links.youtube}>  <Icon scale={iconScale} data={faYoutube} />  </a>
             <a href={links.dev}>      <Icon scale={iconScale} data={faDev} />      </a>
+            <a href={links.github}>   <Icon scale={iconScale} data={faGithub} />   </a>
+            <a href={links.dribbble}> <Icon scale={iconScale} data={faDribbble} /> </a>
+            <a href={links.codepen}>  <Icon scale={iconScale} data={faCodepen} />  </a>
+            <a href={links.twitter}>  <Icon scale={iconScale} data={faTwitter} />  </a>
+            <a href={links.youtube}>  <Icon scale={iconScale} data={faYoutube} />  </a>
+            <a href={links.dev}>      <Icon scale={iconScale} data={faDev} />      </a>
+            <a href={links.github}>   <Icon scale={iconScale} data={faGithub} />   </a>
+            <a href={links.dribbble}> <Icon scale={iconScale} data={faDribbble} /> </a>
+            <a href={links.codepen}>  <Icon scale={iconScale} data={faCodepen} />  </a>
+            <a href={links.twitter}>  <Icon scale={iconScale} data={faTwitter} />  </a>
+            <a href={links.youtube}>  <Icon scale={iconScale} data={faYoutube} />  </a>
         </div>
     </div>
 </section>
