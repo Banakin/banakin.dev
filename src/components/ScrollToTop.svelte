7c2e15b0df823e24966e97c66fb953101f62fecb
Brendan Leu <banakin9@gmail.com>
2021-01-16 23:25:25 -0500

    Finish back to top button

--- src/components/ScrollToTop.svelte
+++ src/components/ScrollToTop.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { fade } from 'svelte/transition';
+
     import * as animateScroll from "svelte-scrollto";
 
     import Icon from 'svelte-awesome';
@@ -10,7 +12,39 @@
 <svelte:window bind:scrollY={y}/>
 
 <style lang="scss">
+@import '../styles/variables';
 
+div {
+    right: 2rem;
+    bottom: 2rem;
+    height: 3rem;
+    width: 3rem;
+    display: block;
+    z-index: 100;
+    border-radius: 100%;
+    color: $black;
+    background-color: $white;
+    position: fixed;
+    line-height: 100%;
+
+    span{
+        width: 100%;
+        text-align: center;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        -ms-transform: translateY(-50%);
+        transform: translateY(-50%);
+    }
+}
 </style>
 
-<div on:click={() => animateScroll.scrollToTop()}><Icon scale=2.5 data={faChevronUp} /></div>
+{#if (y > 0)}
+    <div 
+        transition:fade="{{ duration: 200 }}"
+        on:click={() => animateScroll.scrollToTop()}>
+        <span>
+            <Icon scale=2 data={faChevronUp} />
+        </span>
+    </div>
+{/if}
